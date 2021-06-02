e3b98780887e2b023e70d0f2ec8c1d3b4221fc26
Sk Shahnawaz <skshahnawaz2909@gmail.com>
2021-06-03 02:37:31 +0530

    Redis fixed

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,113 +1,123 @@
 const UploadModel = require("../model/schema");
 const fs = require("fs");
-const Pusher = require ('pusher');
-const cloudinary = require ('cloudinary').v2;
-const redis_client = require ('../../server').getRedisClient();
+const Pusher = require("pusher");
+const cloudinary = require("cloudinary").v2;
+const redis_client = require("../../server").getRedisClient();
 
 let test; //to store the response generated after storing the image in cloudinary
 
-
-//creating the new pusher 
-const pusher = new Pusher ({
+//creating the new pusher
+const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID,
   key: process.env.PUSHER_APP_KEY,
   secret: process.env.PUSHER_APP_SECRET,
-  cluster: process.env.PUSHER_APP_CLUSTER
+  cluster: process.env.PUSHER_APP_CLUSTER,
 });
 
 //to get the redis object
 function getRedis(key) {
   return new Promise((resolve, reject) => {
-   redis_client.get(key, (err, val) => {
-    if (err) {
-     reject(err)
-     return
-    }
-    if (val == null) {
-     resolve(null)
-     return
-    }
- 
-    try {
-     resolve(
-      JSON.parse(val)
-     )
-    } catch (ex) {
-     resolve(val)
-    }
-    redis_client.quit();
-   })
-  })
- }
+    redis_client.get(key, (err, val) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      if (val == null) {
+        resolve(null);
+        return;
+      }
+
+      try {
+        resolve(JSON.parse(val));
+      } catch (ex) {
+        resolve(val);
+      }
+      // redis_client.quit();
+    });
+  });
+}
 
 //function to get the images from the cache
-//takes the attribute for sorting and returns the array of sorted images 
-async function getImagesFromCache (property, order) {
-  let count = await getRedis('image_count');
+//takes the attribute for sorting and returns the array of sorted images
+async function getImagesFromCache(property, order) {
+  let count = await getRedis("image_count");
   //collect the images from the redis into images and return into the home page
-  let images = []
-  for(var key = 0; key < count; key++) {
+  let images = [];
+  for (var key = 0; key < count; key++) {
     let image = await getRedis(key);
     images.push(image);
   }
 
   //sorting the images
-  if(order == 1){
+  if (order == 1) {
     images = images.sort((a, b) => {
-       return (a[property] - b[property]);
+      return a[property] - b[property];
     });
-  }
-  else {
+  } else {
     images = images.sort((a, b) => {
-      return (b[property] - a[property]);
-   });
+      return b[property] - a[property];
+    });
   }
   //modelling the image into an array of  mongoose object
   const all_images = [];
-  for(var i = 0; i < images.length; i++) {
-      all_images.push(UploadModel(images[i]));
+  for (var i = 0; i < images.length; i++) {
+    all_images.push(UploadModel(images[i]));
   }
   return all_images;
 }
 
 //will return the images according to latest arrival
 exports.home = async (req, res) => {
-  const all_images = await getImagesFromCache('postedon', 0);
+  const all_images = await getImagesFromCache("postedon", 0);
   res.render("main", { images: all_images });
 };
 
 //will return images according to upvote count (high - low)
-exports.orderByUpvotes = async (req, res) => {  
-  const all_images = await getImagesFromCache('upvotes', 0);
+exports.orderByUpvotes = async (req, res) => {
+  const all_images = await getImagesFromCache("upvotes", 0);
   res.render("main", { images: all_images });
 };
 
 //will return images according to downvote count (low - high)
-exports.orderByDownvotes = async (req, res) => {  
-  const all_images = await getImagesFromCache('downvotes', 1);
+exports.orderByDownvotes = async (req, res) => {
+  const all_images = await getImagesFromCache("downvotes", 1);
   res.render("main", { images: all_images });
 };
 
 //to update the upvote count in the meme and the notify other clients involved about the change
 exports.updateVotes = (req, res, next) => {
   const action = req.body.action;
-  if (action == "Upvote") { //increment the count of the upvotes  
+  if (action == "Upvote") {
+    //increment the count of the upvotes
     UploadModel.updateOne(
-      { _id: req.params.id }, 
+      { _id: req.params.id },
       { $inc: { upvotes: 1 } },
       {},
-      (err, numberAffected) => { //generating a trigger to let other connected nodes about the change
-        pusher.trigger('post-events', 'postAction', {action: action, postId: req.params.id}, req.body.socketId);
-        res.send ('');
+      (err, numberAffected) => {
+        //generating a trigger to let other connected nodes about the change
+        pusher.trigger(
+          "post-events",
+          "postAction",
+          { action: action, postId: req.params.id },
+          req.body.socketId
+        );
+        res.send("");
       }
     );
-  } else { //increment the count of downvotes
+  } else {
+    //increment the count of downvotes
     UploadModel.updateOne(
       { _id: req.params.id },
       { $inc: { downvotes: 1 } },
       {},
-      (err, numberAffected) => { //generating a pusher trigger 
-        pusher.trigger('post-events', 'postAction', {action: action, postId: req.params.id}, req.body.socketId);
+      (err, numberAffected) => {
+        //generating a pusher trigger
+        pusher.trigger(
+          "post-events",
+          "postAction",
+          { action: action, postId: req.params.id },
+          req.body.socketId
+        );
         res.send("");
       }
     );
@@ -123,18 +133,18 @@ exports.uploads = (req, res, next) => {
     error.httpStatusCode = 400;
     return next(error);
   }
-  cloudinary.config ({
-      cloud_name: 'meme-o-gram',
-      api_key: '953418367462681',
-      api_secret: 'l1ri-DEF76EW9dm5cIRUjSphD8M'
-  })
-  //upload the image url from cloudinary 
-  let imgArray =  files.map(async (file) => {
-      let img = fs.readFileSync(file.path);
-      test = await cloudinary.uploader.upload (file.path, (err, result) => {
-        //for debugging purposes
+  cloudinary.config({
+    cloud_name: "meme-o-gram",
+    api_key: "953418367462681",
+    api_secret: "l1ri-DEF76EW9dm5cIRUjSphD8M",
+  });
+  //upload the image url from cloudinary
+  let imgArray = files.map(async (file) => {
+    let img = fs.readFileSync(file.path);
+    test = await cloudinary.uploader.upload(file.path, (err, result) => {
+      //for debugging purposes
     });
-    //modelling the mongoose object 
+    //modelling the mongoose object
     let finalImg = {
       username: username,
       filename: file.originalname,
@@ -146,7 +156,7 @@ exports.uploads = (req, res, next) => {
     return newUpload
       .save()
       .then(() => {
-        res.redirect ('/');
+        res.redirect("/");
         return { msg: `${file.originalname} Uploaded Successfully..!` };
       })
       .catch((error) => {
@@ -162,6 +172,5 @@ exports.uploads = (req, res, next) => {
             `Cannot Upload ${file.originalname} something missing`,
         });
       });
-    
   });
 };
--- server/router/router.js
+++ server/router/router.js
@@ -1,11 +1,11 @@
-const route = require ('express').Router()
-const controller = require('../controller/controller');
-const store = require ('../middleware/multer')
+const route = require("express").Router();
+const controller = require("../controller/controller");
+const store = require("../middleware/multer");
 //routes
-route.get ('/', controller.home);
+route.get("/", controller.home);
 //maximums 12 images can be posted at a time
-route.post ('/uploadmultiple', store.array('images', 12), controller.uploads);
-route.post ('/posts/:id/act', controller.updateVotes); 
-route.get ('/orderbyupvotes', controller.orderByUpvotes); //route handler for handling the sort by feature based on upvote count on a post (high -low)
-route.get ('/orderbydownvotes', controller.orderByDownvotes); //route handler for handling the sort by feature based on downvote count on a post (low - high)
+route.post("/uploadmultiple", store.array("images", 12), controller.uploads);
+route.post("/posts/:id/act", controller.updateVotes);
+route.get("/orderbyupvotes", controller.orderByUpvotes); //route handler for handling the sort by feature based on upvote count on a post (high -low)
+route.get("/orderbydownvotes", controller.orderByDownvotes); //route handler for handling the sort by feature based on downvote count on a post (low - high)
 module.exports = route;
