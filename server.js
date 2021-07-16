d4f18766135b729ef981c65ae53ecb041d9a21a0
debargham14 <debargham14@gmail.com>
2021-07-16 13:47:41 +0530

    slight modification

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 //creating the http server using js
 const express = require("express");
-const redis = require("redis");
+//const redis = require("redis");
 
 require("dotenv").config();
 
-
 const hbs = require("express-handlebars");
-const handlebars = require('hbs');
+const handlebars = require("hbs");
 const path = require("path");
-const passport = require('passport');
-const flash = require('connect-flash');
-const session = require('express-session');
-
+const passport = require("passport");
+const flash = require("connect-flash");
+const session = require("express-session");
 
 const app = express();
 
-require('./server/passport/passport')(passport);
+require("./server/passport/passport")(passport);
 
 app.use(express.json()); //allows to post the json data when we make a post request
 
@@ -39,14 +37,14 @@ app.engine(
 );
 
 //Bodyparser
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 //Express session
 app.use(
   session({
-    secret: 'secret',
+    secret: "secret",
     resave: true,
-    saveUninitialized: true
+    saveUninitialized: true,
   })
 );
 
@@ -57,47 +55,47 @@ app.use(flash());
 
 //Global variables
 app.use((req, res, next) => {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
   next();
 });
 
-//setting up the redis port
-const port_redis = process.env.PORT || 6379;
+// //setting up the redis port
+// const port_redis = process.env.PORT || 6379;
 const port = process.env.PORT || 8080;
 
 //configuring the redis client on port 6379
-const redis_client = redis.createClient({
-  port: port_redis,
-  host: "localhost",
-});
+// const redis_client = redis.createClient({
+//   port: port_redis,
+//   host: "localhost",
+// });
 
-redis_client.on("error", (error) => {
-  console.log(error.message);
-});
+// redis_client.on("error", (error) => {
+//   console.log(error.message);
+// });
 
-redis_client.on("connect", () => {
-  console.log("Successfully connected to redis");
-});
+// redis_client.on("connect", () => {
+//   console.log("Successfully connected to redis");
+// });
 
-async function dataFetch() {
-  const all_images = await UploadModel.find().sort({ postedon: -1 });
-  for (var idx = 0; idx < Math.min(all_images.length, 10); idx++) {
-    redis_client.setex(idx, 3600, JSON.stringify(all_images[idx]));
-  }
-  //to store the image count of the
-  redis_client.setex("image_count", 3600, Math.min(all_images.length, 10));
-  return 1;
-}
+// async function dataFetch() {
+//   const all_images = await UploadModel.find().sort({ postedon: -1 });
+//   for (var idx = 0; idx < Math.min(all_images.length, 10); idx++) {
+//     redis_client.setex(idx, 3600, JSON.stringify(all_images[idx]));
+//   }
+//   //to store the image count of the
+//   redis_client.setex("image_count", 3600, Math.min(all_images.length, 10));
+//   return 1;
+// }
 
-setInterval(async () => {
-  let check = await dataFetch();
-}, 3000);
+// setInterval(async () => {
+//   let check = await dataFetch();
+// }, 3000);
 
-exports.getRedisClient = () => {
-  return redis_client;
-};
+// exports.getRedisClient = () => {
+//   return redis_client;
+// };
 
 //calling routes
 app.use("/", require("./server/router/router"));
@@ -105,17 +103,18 @@ const server = app.listen(port, () =>
   console.log(`Server is stated on http://localhost:${port}`)
 );
 
-let io = require('socket.io')(server);
+let io = require("socket.io")(server);
 
-io.on ('connection', (socket) => {
-   console.log(`New Connection: ${socket.id}`);
-   
-   //Receive events
-   socket.on('comment', (data) => {
-      socket.broadcast.emit('comment', data);
-   })
+io.on("connection", (socket) => {
+  console.log(`New Connection: ${socket.id}`);
+  console.log("Connection Port: ", socket.port);
 
-   socket.on('Vote', (data) => {
-     socket.broadcast.emit('Vote', data);
-   })
-})
+  //Receive events
+  socket.on("comment", (data) => {
+    socket.broadcast.emit("comment", data);
+  });
+
+  socket.on("Vote", (data) => {
+    socket.broadcast.emit("Vote", data);
+  });
+});
--- server/controller/controller.js
+++ server/controller/controller.js
@@ -2,25 +2,25 @@ const UploadModel = require("../model/schema");
 const fs = require("fs");
 const Pusher = require("pusher");
 const cloudinary = require("cloudinary").v2;
-const redis_client = require("../../server").getRedisClient();
-const handlebars = require('handlebars');
+//const redis_client = require("../../server").getRedisClient();
+const handlebars = require("handlebars");
 
 let test; //to store the response generated after storing the image in cloudinary
 
-handlebars.registerHelper('returnUpvoteCount', (v) => {
+handlebars.registerHelper("returnUpvoteCount", (v) => {
   return v.upvotes.length;
 });
 
-handlebars.registerHelper('returnDownvoteCount', (v) => {
+handlebars.registerHelper("returnDownvoteCount", (v) => {
   return v.downvotes.length;
 });
 
-handlebars.registerHelper('returnVoteId', (v) => {
-    let str;
-    for(let i = 0; i < v; i++) {
-        str = str + v[i] + " ";
-    }
-    return str;
+handlebars.registerHelper("returnVoteId", (v) => {
+  let str;
+  for (let i = 0; i < v; i++) {
+    str = str + v[i] + " ";
+  }
+  return str;
 });
 //creating the new pusher
 const pusher = new Pusher({
@@ -29,86 +29,95 @@ const pusher = new Pusher({
   secret: process.env.PUSHER_APP_SECRET,
   cluster: process.env.PUSHER_APP_CLUSTER,
 });
-handlebars.registerHelper('loop', (v1) => {
-  for(var i = 0; i < v1.length; i++) {
-    
-  }
+handlebars.registerHelper("loop", (v1) => {
+  for (var i = 0; i < v1.length; i++) {}
 });
-//to get the redis object
-function getRedis(key) {
-  return new Promise((resolve, reject) => {
+// //to get the redis object
+// function getRedis(key) {
+//   return new Promise((resolve, reject) => {
 
-   redis_client.get(key, (err, val) => {
-    if (err) {
-     reject(err)
-     return
-    }
-    if (val == null) {
-     resolve(null)
-     return
-    }
- 
-    try {
-     resolve(
-      JSON.parse(val)
-     )
-    } catch (ex) {
-     resolve(val)
-    }
-   })
-  })
- }
+//    redis_client.get(key, (err, val) => {
+//     if (err) {
+//      reject(err)
+//      return
+//     }
+//     if (val == null) {
+//      resolve(null)
+//      return
+//     }
 
-//function to get the images from the cache
-//takes the attribute for sorting and returns the array of sorted images
-async function getImagesFromCache(property, order) {
-  let count = await getRedis("image_count");
-  //collect the images from the redis into images and return into the home page
-  let images = [];
-  for (var key = 0; key < count; key++) {
-    let image = await getRedis(key);
-    images.push(image);
-  }
+//     try {
+//      resolve(
+//       JSON.parse(val)
+//      )
+//     } catch (ex) {
+//      resolve(val)
+//     }
+//    })
+//   })
+//  }
 
-  //sorting the images
-  if (order == 1) {
-    images = images.sort((a, b) => {
-      return a[property] - b[property];
-    });
-  } else {
-    images = images.sort((a, b) => {
-      return b[property] - a[property];
-    });
-  }
-  //modelling the image into an array of  mongoose object
-  const all_images = [];
-  for (var i = 0; i < images.length; i++) {
-    all_images.push(UploadModel(images[i]));
-  }
-  return all_images;
-}
+// //function to get the images from the cache
+// //takes the attribute for sorting and returns the array of sorted images
+// async function getImagesFromCache(property, order) {
+//   let count = await getRedis("image_count");
+//   //collect the images from the redis into images and return into the home page
+//   let images = [];
+//   for (var key = 0; key < count; key++) {
+//     let image = await getRedis(key);
+//     images.push(image);
+//   }
+
+//   //sorting the images
+//   if (order == 1) {
+//     images = images.sort((a, b) => {
+//       return a[property] - b[property];
+//     });
+//   } else {
+//     images = images.sort((a, b) => {
+//       return b[property] - a[property];
+//     });
+//   }
+//   //modelling the image into an array of  mongoose object
+//   const all_images = [];
+//   for (var i = 0; i < images.length; i++) {
+//     all_images.push(UploadModel(images[i]));
+//   }
+//   return all_images;
+// }
 
 //will return the images according to latest arrival
 exports.home = async (req, res) => {
-  const all_images = await UploadModel.find().sort({postedon: -1});
+  const all_images = await UploadModel.find().sort({ postedon: -1 });
   const entering_user = req.user;
-  res.render("dashboard", { images: all_images, username: entering_user.username, uid: entering_user.id});
+  res.render("dashboard", {
+    images: all_images,
+    username: entering_user.username,
+    uid: entering_user.id,
+  });
 };
 
 //will return images according to upvote count (high - low)
 exports.orderByUpvotes = async (req, res) => {
-  const all_images = await UploadModel.find().sort({upvotes: -1});
+  const all_images = await UploadModel.find().sort({ upvotes: -1 });
   const entering_user = req.user;
-  res.render("dashboard", { images: all_images, username: entering_user.username, uid: entering_user.id});
+  res.render("dashboard", {
+    images: all_images,
+    username: entering_user.username,
+    uid: entering_user.id,
+  });
 };
 
 //will return images according to downvote count (low - high)
 exports.orderByDownvotes = async (req, res) => {
-  const all_images = await UploadModel.find().sort({downvotes: 1});
+  const all_images = await UploadModel.find().sort({ downvotes: 1 });
   const entering_user = req.user;
-  res.render("dashboard", { images: all_images, username: entering_user.username, uid: entering_user.id });
+  res.render("dashboard", {
+    images: all_images,
+    username: entering_user.username,
+    uid: entering_user.id,
+  });
 };
-
 
 //to update the upvote count in the meme and the notify other clients involved about the change
 exports.updateVotes = (req, res, next) => {
@@ -120,19 +129,17 @@ exports.updateVotes = (req, res, next) => {
       { _id: req.params.id },
       { $addToSet: { upvotes: uid } },
       (err, success) => {
-        if(err)
-          console.log(err);
-      } 
+        if (err) console.log(err);
+      }
     );
   } else {
     //increment the count of downvotes
     UploadModel.updateOne(
       { _id: req.params.id },
       { $addToSet: { downvotes: uid } },
-       (err, success) => {
-         if(err) 
-          console.log(err);
-       }
+      (err, success) => {
+        if (err) console.log(err);
+      }
     );
   }
 };
@@ -191,24 +198,23 @@ exports.uploads = (req, res, next) => {
 
 //update the comment for the particular post
 exports.updateComments = (req, res) => {
-    //update the comments for a particular post
-    let username = req.body.username;
-    let uid = req.body.uid;
-    let comment = req.body.comment;
-    let postId = req.body.postId;
-    let obj = {
-      username: username,
-      uid: uid,
-      comment: comment,
-      postId: postId
-    }
+  //update the comments for a particular post
+  let username = req.body.username;
+  let uid = req.body.uid;
+  let comment = req.body.comment;
+  let postId = req.body.postId;
+  let obj = {
+    username: username,
+    uid: uid,
+    comment: comment,
+    postId: postId,
+  };
 
-    UploadModel.updateOne(
-      {_id: postId},
-      {$push: {comments: obj}},
-      (error, success) => {
-        if(error)
-          console.log(error);
-      }
-    );
-}
+  UploadModel.updateOne(
+    { _id: postId },
+    { $push: { comments: obj } },
+    (error, success) => {
+      if (error) console.log(error);
+    }
+  );
+};
